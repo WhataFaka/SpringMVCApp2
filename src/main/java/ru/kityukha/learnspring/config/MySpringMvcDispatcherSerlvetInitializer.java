88a482214e28b5892e40ec4be9b8fa2fdac4cbbe
Your Name <mrmofd@gmail.com>
2023-01-12 00:01:05 +0700

    controllers

---
+++ src/main/java/ru/kityukha/learnspring/config/MySpringMvcDispatcherSerlvetInitializer.java
@@ -0,0 +1,21 @@
+package ru.kityukha.learnspring.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MySpringMvcDispatcherSerlvetInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
